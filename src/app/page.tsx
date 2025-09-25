7f55847ba969263904b378bc19c607b67b4c8cb5
xxbitencourt <vinicius.botelho@estudante.ifms.edu.br>
2025-09-25 11:30:14 -0400

    commit
    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button>Button</Button>
+    </div>
+  )
+}
