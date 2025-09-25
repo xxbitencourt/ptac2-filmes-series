60658d03613af35ba8f9c0116673fc96152b4ee1
xxbitencourt <119963638+xxbitencourt@users.noreply.github.com>
2025-09-25 23:17:19 +0000

    inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardHeader,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap items-center gap-2 md:flex-row">
-      <Button>Button</Button>
-      <p>Texto</p>
+    <div className="w-full max-w-sm">
+      <Card className="w-full">
+        <CardHeader className="p-0">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BOGMwZGJiM2EtMzEwZC00YTYzLWIxNzYtMmJmZWNlZjgxZTMwXkEyXkFqcGc@._V1_.jpg"
+            alt="Imagem do card"
+            loading="lazy"
+            className="w-full h-56 object-cover rounded-lg"
+          />
+        </CardHeader>
+      </Card>
+
+      {/* Descrição fora do card */}
+      <p className="mt-2 text-sm text-muted-foreground">
+        Aqui vai a descrição da imagem ou do conteúdo do card.
+      </p>
     </div>
   )
 }
