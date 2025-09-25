0344d54dadf84721033655c14801bbc7d60c50a9
xxbitencourt <119963638+xxbitencourt@users.noreply.github.com>
2025-09-25 15:34:04 +0000

    teste commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
       <Button>Button</Button>
+      <p>Texto</p>
     </div>
   )
 }
